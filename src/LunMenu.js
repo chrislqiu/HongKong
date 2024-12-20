1de505c9a2b7d4664a8c9332a96ec8f68fcd67a5
Chris <chris.qiu52@gmail.com>
2024-12-20 16:39:09 -0500

    adding lunch items

---
+++ src/LunMenu.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import './LunMenu.css';
+
+const menuData = [
+    {
+      category: 'Chicken',
+      items: [
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+      ],
+    },
+    {
+    category: 'Beef',
+    items: [
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+    ]
+    },
+        {
+    category: 'Shrimp',
+    items: [
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+    ]
+    },
+        {
+    category: 'Pork',
+    items: [
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+    ]
+    },
+        {
+    category: 'Extra',
+    items: [
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+        { id: 'L', name: '', price: '7.29', spicy: false},
+    ]
+    },
+]
