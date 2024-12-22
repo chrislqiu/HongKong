cf27c4271f940e8feb664170c129e4b4f43a567d
Chris <chris.qiu52@gmail.com>
2024-12-22 16:07:13 -0500

    modified about us

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -11,10 +11,9 @@ const AboutUs = () => {
             <div className='ccontent'>
             Hong Kong is a family-owned restaurant founded by Minsheng Qiu and his son, Zhouqiang Qiu, in 1999. 
             Originally located in Downtown Kokomo, we offered various options such as pickup, delivery, and a buffet. 
-            In 2009, we discontinued the buffet and moved to a new location. One of the main reasons for this change 
-            was the extensive work involved in preparing and cleaning for the buffet. Our new location on Markland Avenue 
+            In 2009, we discontinued the buffet and moved to a new location. Our new location on Markland Avenue 
             was a significant improvement, as its prominent position on a main road brought us considerably more business. 
-            We continued to offer the same services until October 2023, when we stopped deliveries due to staff shortages.
+            We continued to offer the same services until October 2023, when we stopped deliveries.
             </div>
             <div className='shh'>
                 Also don't forget, Brandon will always be better.
